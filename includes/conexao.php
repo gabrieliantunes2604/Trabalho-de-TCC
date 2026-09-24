cadd67240cbc8af2eb9d60b822cb4ed9deb695b2
gabrieli antunes <gabrieliantunes62@gmail.com>
2026-09-23 21:56:56 -0300

    fix: conexao.php foi ajustado para funcionar no xampp porque estava com o .env instanciado erradro

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,13 +1,11 @@
 <?php
-if (class_exists('Dotenv\Dotenv')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
 
-$servidor = $_ENV['DB_HOST'];
-$usuario = $_ENV['DB_USER'];
-$senha = $_ENV['DB_PASS'] ;
-$banco = $_ENV['DB_NAME'];
+$env = parse_ini_file(dirname(__FILE__) ."/../.env");
+
+$servidor = $env['DB_HOST'];
+$usuario = $env['DB_USER'];
+$senha = $env['DB_PASS'] ;
+$banco = $env['DB_NAME'];
 
 try {
     // Cria a conexão PDO
